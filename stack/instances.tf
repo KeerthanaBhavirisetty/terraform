654f50cfc284e4d6d30fecda355daeae15bc2ad2
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-01 06:37:05 +0100

    --- Auto-Git Commit ---

--- stack/instances.tf
+++ stack/instances.tf
@@ -1,8 +1,19 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "node1" {
   ami           = "ami-07d19c1aefc450697"
   instance_type = "t2.micro"
+  key_name      = "devops"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Instance1"
+  }
+}
+
+resource "aws_instance" "node2" {
+  ami           = "ami-07d19c1aefc450697"
+  instance_type = "t2.micro"
+  key_name      = "devops"
+
+  tags = {
+    Name = "Instance2"
   }
 }
