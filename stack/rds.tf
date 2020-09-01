db6c56b8e8599c27eec4b294be9bf6a1837c52cb
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-01 06:25:14 +0100

    --- Auto-Git Commit ---

--- stack/rds.tf
+++ stack/rds.tf
@@ -2,10 +2,10 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mariadb"
-  engine_version       = "5.7"
+  engine_version       = "10.4"
   instance_class       = "db.t2.micro"
   name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mariadb10.4"
 }
