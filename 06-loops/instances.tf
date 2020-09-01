451ffd1c38f84bedac4a642a69fb98e5d8629946
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-01 06:54:35 +0100

    --- Auto-Git Commit ---

--- 06-loops/instances.tf
+++ 06-loops/instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "node" {
   count         = 2
   ami           = "ami-07d19c1aefc450697"
   instance_type = "t2.micro"
-  key_name      = "devops"
+  key_name      = "key_28042020"
 
   tags = {
     Name = "Instance"
