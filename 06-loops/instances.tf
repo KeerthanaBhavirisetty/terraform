59287047dcbe3a10bfaca7bef552d0306e8aaa54
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-01 07:00:05 +0100

    --- Auto-Git Commit ---

--- 06-loops/instances.tf
+++ 06-loops/instances.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "node" {
   ami           = "ami-07d19c1aefc450697"
   instance_type = "t2.micro"
   key_name      = "key_28042020"
-  security_groups = "sg-a89c67f6"
+  security_groups = "MySecurityGrp"
 
   tags = {
     Name = "Instance"
