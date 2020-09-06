99569b112da0b01e544d99a398f8e464fc0c8387
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-06 11:05:59 +0100

    --- Auto-Git Commit ---

--- stack/provider.tf
+++ stack/provider.tf
@@ -6,7 +6,7 @@ terraform {
     
   backend "s3" {
     bucket = "state-info-bucket"
-    key    = "stack/terraform.tfstate"
+    key    = "remote-state/terraform.tfstate"
     region = "us-east-1"
   }
 }
