529a3a8268b7fafde6e4ad6aefb6ae222f7de08f
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-06 08:34:55 +0100

    --- Auto-Git Commit ---

--- 07-data/instances.tf
+++ 07-data/instances.tf
@@ -3,8 +3,7 @@ resource "aws_instance" "node" {
   ami           = "ami-07d19c1aefc450697"
   instance_type = "t2.micro"
   key_name      = "key_28042020"
-  for_each      = data.aws_subnet_ids.example.ids
-  subnet_id     = each.value
+  subnet_id     = "${data.aws_subnet_ids.subnets.ids[count.index]}"
 
   tags = {
     Name = "Instance-${count.index+1}"
