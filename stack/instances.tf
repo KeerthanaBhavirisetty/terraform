9f614cf4c16880040497a33a4fbc2b92a281d697
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-06 10:27:18 +0100

    --- Auto-Git Commit ---

--- stack/instances.tf
+++ stack/instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "node" {
   count         = 2
-  ami           = "ami-07d19c1aefc450697"
+  ami           = "vars.ami"
   instance_type = "t2.micro"
   key_name      = "devops"
   subnet_id     = sort(data.aws_subnet_ids.subnets.ids)[count.index]
