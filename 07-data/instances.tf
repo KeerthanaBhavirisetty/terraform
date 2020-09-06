5ab117db9d1be5a882874b509ceae57c623a3e5f
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-06 08:13:33 +0100

    --- Auto-Git Commit ---

---
+++ 07-data/instances.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "node" {
+  count         = 2
+  ami           = "ami-07d19c1aefc450697"
+  instance_type = "t2.micro"
+  key_name      = "key_28042020"
+
+  tags = {
+    Name = "Instance-${count.index+1}"
+  }
+}
---
+++ 07-data/provider.tf
@@ -0,0 +1,10 @@
+provider "aws"{}
+
+terraform {
+    
+  backend "s3" {
+    bucket = "state-info-bucket"
+    key    = "remote-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
