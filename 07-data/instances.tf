5a20fecff3f0c8b8e6fcfe3f8e00102b18b7a10d
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-06 09:56:12 +0100

    --- Auto-Git Commit ---

--- 07-data/instances.tf
+++ 07-data/instances.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "node" {
   ami           = "ami-07d19c1aefc450697"
   instance_type = "t2.micro"
   key_name      = "key_28042020"
-  subnet_id     = sort(data.aws_subnet_ids.subnets)[count.index]
+  subnet_id     = sort(data.aws_subnet_ids.subnets.ids)[count.index]
   #subnet_id     = sort(data.terraform_remote_state.network.outputs.public_subnet_list)[count.index]
 
   tags = {
