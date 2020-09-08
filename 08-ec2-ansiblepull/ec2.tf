1f47b2665e188f6d6538387ad89ab326242eaf81
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-08 06:37:31 +0100

    --- Auto-Git Commit ---

--- 08-ec2-ansiblepull/ec2.tf
+++ 08-ec2-ansiblepull/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "node"{
   ami           = "ami-07d19c1aefc450697"
   instance_type = "t2.micro"
   key_name      = "key_28042020"
+  vpc_security_group_ids = "sg-0fd363c7a673b5e7c"
 
   tags = {
     Name = "Instance-1"
