4f03dad679dcc343ffa946ec03a2b77e7bf5a549
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-10 17:01:53 +0100

    --- Auto-Git Commit ---

--- 09-modules/Modules/ec2/instance.tf
+++ 09-modules/Modules/ec2/instance.tf
@@ -1,10 +1,6 @@
-resource "aws_instance" "node"{
-  ami           = "ami-07d19c1aefc450697"
-  instance_type = "t2.micro"
-  key_name      = "key_28042020"
-  vpc_security_group_ids = ["sg-0fd363c7a673b5e7c"]
+resource null_resource null1 {
 
-  tags = {
-    Name = "Instance-1"
+  provisioner "local-exec" {
+    command = "echo ${var.s3-bucket} >> private_ips.txt"
   }
 }
