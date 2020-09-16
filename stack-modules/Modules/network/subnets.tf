86c2ba68d71c0e083b99e4e1d5137611d0f956be
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-16 19:02:03 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/subnets.tf
+++ stack-modules/Modules/network/subnets.tf
@@ -2,7 +2,6 @@ resource "aws_subnet" "main" {
     count       = 4
     vpc_id      = aws_vpc.studentapp-vpc.id
     cidr_block  = cidrsubnet("10.0.0.0/21", 3, 2)
-    availability_zone = var.AZ[count.index]
 
   tags = {
     Name = "Main"
