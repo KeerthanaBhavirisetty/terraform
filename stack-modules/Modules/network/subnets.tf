542043453da4c2dec07e4a9eec60c55aba224502
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-16 19:05:53 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/subnets.tf
+++ stack-modules/Modules/network/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
     count       = 4
     vpc_id      = aws_vpc.studentapp-vpc.id
-    cidr_block  = cidrsubnet("10.0.0.0/21", 3, count.index)
+    cidr_block  = cidrsubnet(var.VPC_CIDR, 3, count.index)
 
   tags = {
     Name = "Main"
