958874553e772b4aae7114dfccd3c422bdb23397
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-16 06:57:44 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/vpc.tf
+++ stack-modules/Modules/network/vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "studentapp-vpc" {
-  cidr_block       = "10.0.0.0/21"
+  cidr_block       = var.VPC_CIDR
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = var.PROJECT_NAME-var.PROJECT_ENV-VPC
   }
 }
