27ce23f1ca5b34f66522c37e42ca7412f7f3e37b
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-16 20:01:49 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/internet_gateway.tf
+++ stack-modules/Modules/network/internet_gateway.tf
@@ -2,6 +2,10 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.studentapp-vpc.id
 
   tags = {
-    Name = "main"
+     Name         = "${var.PROJECT_NAME}-${var.PROJECT_ENV}-IGW"
+     created_by   = "Terraform"
+     PROJECT_NAME = var.PROJECT_NAME
+     ENVIRONMENT  = var.PROJECT_ENV
+    
   }
 }
