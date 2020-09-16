31151a58d5989f8817e15713d66abb06585cdaf7
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-16 20:12:12 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/subnets.tf
+++ stack-modules/Modules/network/subnets.tf
@@ -26,3 +26,21 @@ resource "aws_subnet" "private-subnets" {
      CREATED_BY   = "Terraform"
   }
 }
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.studentapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.foo.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
