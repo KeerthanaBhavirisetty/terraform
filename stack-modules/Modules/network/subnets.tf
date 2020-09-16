7a1530b66779573d06ae125e7dccfa4124f4720e
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-16 19:03:43 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/subnets.tf
+++ stack-modules/Modules/network/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "main" {
     count       = 4
     vpc_id      = aws_vpc.studentapp-vpc.id
-    cidr_block  = cidrsubnet("10.0.0.0/21", 3, 2)
+    cidr_block  = cidrsubnet("10.0.0.0/21", 3, count.index)
 
   tags = {
     Name = "Main"
