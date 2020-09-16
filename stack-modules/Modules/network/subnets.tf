826d8b62f7ea27f27dca7ea032fd7f2aaee8aa9d
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-16 19:31:15 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/subnets.tf
+++ stack-modules/Modules/network/subnets.tf
@@ -5,6 +5,17 @@ resource "aws_subnet" "public-subnets" {
     availability_zone = element(var.AZ,count.index)
 
   tags = {
-    Name = "Main"
+    Name = "public"
+  }
+}
+
+resource "aws_subnet" "private-subnets" {
+    count       = 4
+    vpc_id      = aws_vpc.studentapp-vpc.id
+    cidr_block  = cidrsubnet(var.VPC_CIDR, 3, count.index)
+    availability_zone = element(var.AZ,count.index)
+
+  tags = {
+    Name = "private"
   }
 }
