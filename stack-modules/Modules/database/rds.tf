e1217abfb0c5aaa0694c2f61415fe312c3591db6
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 05:23:35 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/database/rds.tf
+++ stack-modules/Modules/database/rds.tf
@@ -1,13 +1,13 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 20
+  allocated_storage    = var.DB_SIZE
   storage_type         = "gp2"
-  engine               = "mariadb"
-  engine_version       = "10.4"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
+  engine               = var.DB_ENGINE
+  engine_version       = var.DB_ENGINE_VERSION
+  instance_class       = var.DB_INSTANCE
+  name                 = var.DB_NAME
+  username             = var.DB_USER
+  password             = var.DB_PASS
   parameter_group_name = "default.mariadb10.4"
-  skip_final_snapshot  = true
-  identifier           = "studentapp-db"
+  skip_final_snapshot  = var.NEED_FINAL_SNAPSHOT
+  identifier           = var.DB_NAME"-db"
 }
