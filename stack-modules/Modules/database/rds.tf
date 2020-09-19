d9181b7b61b9921515b450d10c4bbfe06b6d8ace
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 05:50:30 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/database/rds.tf
+++ stack-modules/Modules/database/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_rds_cluster_parameter_group" "default" {
   name        = "${var.DB_ENGINE}_parameter_group"
-  family      = format(var.DB_ENGINE%s,var.DB_ENGINE_VERSION)
+  family      = format(var.DB_ENGINE%s, var.DB_ENGINE_VERSION)
   description = "RDS default cluster parameter group"
 
   parameter {
