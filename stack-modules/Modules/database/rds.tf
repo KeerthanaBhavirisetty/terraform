008b0916fd4f7a51ddd483518ea7af59e402a32a
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 05:54:40 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/database/rds.tf
+++ stack-modules/Modules/database/rds.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "default" {
   name                 = var.DB_NAME
   username             = var.DB_USER
   password             = var.DB_PASS
-  parameter_group_name = "default.mariadb10.4"
+  parameter_group_name = aws_rds_cluster_parameter_group.default.id
   skip_final_snapshot  = var.NEED_FINAL_SNAPSHOT
   identifier           = "${var.DB_NAME}-db"
 }
