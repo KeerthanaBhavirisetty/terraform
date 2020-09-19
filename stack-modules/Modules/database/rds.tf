6c453bf73c7fee977cee4d9b5825ed193dec5296
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 06:29:06 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/database/rds.tf
+++ stack-modules/Modules/database/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_rds_cluster_parameter_group" "default" {
   name        = "${var.DB_ENGINE}-parameter-group"
-  family      = "${var.DB_ENGINE}${var.DB_ENGINE_VERSION}"
+  family      = "mariadb10.4"
   description = "RDS ${var.DB_ENGINE} cluster parameter group"
 }
 
