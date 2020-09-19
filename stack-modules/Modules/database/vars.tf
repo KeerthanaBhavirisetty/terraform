3db2c45512804481817b1867d3ab0071f3b2a7be
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 05:20:44 +0100

    --- Auto-Git Commit ---

---
+++ stack-modules/Modules/database/vars.tf
@@ -0,0 +1,8 @@
+variable DB_SIZE {}
+variable DB_ENGINE {}
+variable DB_ENGINE_VERSION {}
+variable NEED_FINAL_SNAPSHOT {}
+variable DB_USER {}
+variable DB_NAME {}
+variable DB_PASS {}
+variable DB_INSTANCE {}
