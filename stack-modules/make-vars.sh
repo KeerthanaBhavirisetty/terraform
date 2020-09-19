75facf2a3d84cb9359cffff51e730a0a16c94bab
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 05:17:23 +0100

    --- Auto-Git Commit ---

--- stack-modules/make-vars.sh
+++ stack-modules/make-vars.sh
@@ -3,3 +3,8 @@ file=$1
 for i in `cat $file grep -v '^#' | awk -F = '{print $1}'`; do
     echo -e "variable $i" '{}'
 done
+
+for i in `cat $file grep -v '^#' | awk -F = '{print $1}'`; do
+    echo -e "$i = var.$i" 
+done
+
