09d06c8daddb85faf5f08d0f64c669c44a111b68
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 06:15:35 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/database/rds.tf
+++ stack-modules/Modules/database/rds.tf
@@ -12,16 +12,3 @@ resource "aws_db_subnet_group" "default" {
   }
 }
 
-resource "aws_db_instance" "default" {
-  allocated_storage    = var.DB_SIZE
-  storage_type         = "gp2"
-  engine               = var.DB_ENGINE
-  engine_version       = var.DB_ENGINE_VERSION
-  instance_class       = var.DB_INSTANCE
-  name                 = var.DB_NAME
-  username             = var.DB_USER
-  password             = var.DB_PASS
-  parameter_group_name = aws_rds_cluster_parameter_group.default.id
-  skip_final_snapshot  = !var.NEED_FINAL_SNAPSHOT
-  identifier           = "${var.DB_NAME}-db"
-}
