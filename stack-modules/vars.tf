95ea654ca01d664f9eaa860431e9d7cb9f9da8c7
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-19 05:16:33 +0100

    --- Auto-Git Commit ---

--- stack-modules/vars.tf
+++ stack-modules/vars.tf
@@ -4,3 +4,12 @@ variable VPC_CIDR {}
 variable AZ {
     type = list
 }
+
+variable DB_SIZE {}
+variable DB_ENGINE {}
+variable DB_ENGINE_VERSION {}
+variable NEED_FINAL_SNAPSHOT {}
+variable DB_USER {}
+variable DB_NAME {}
+variable DB_PASS {}
+variable DB_INSTANCE {}
