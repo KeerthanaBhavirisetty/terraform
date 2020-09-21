c118431160202cc53fbcda8e97032c6872002ca9
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-21 05:16:38 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/network/peering.tf
+++ stack-modules/Modules/network/peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "peer" {
   vpc_id        = aws_vpc.studentapp-vpc.id
-  peer_vpc_id   = data.aws_vpcs.current.ids
+  peer_vpc_id   = data.aws_vpc.current.id
   peer_owner_id = data.aws_caller_identity.current.account_id
   auto_accept   = true
 
