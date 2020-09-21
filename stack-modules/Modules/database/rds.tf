e701f2d27de92ac992a721fa7463044a5a28f2b5
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-21 06:10:21 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/database/rds.tf
+++ stack-modules/Modules/database/rds.tf
@@ -43,19 +43,19 @@ resource "aws_security_group" "allow_rds" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage    = var.DB_SIZE
-  storage_type         = "gp2"
-  engine               = var.DB_ENGINE
-  engine_version       = var.DB_ENGINE_VERSION
-  instance_class       = var.DB_INSTANCE
-  name                 = var.DB_NAME
-  username             = var.DB_USER
-  password             = var.DB_PASS
-  parameter_group_name = aws_db_parameter_group.default.id
-  skip_final_snapshot  = !var.NEED_FINAL_SNAPSHOT
-  identifier           = "${var.DB_NAME}-db"
-  db_subnet_group_name = aws_db_subnet_group.default.id
-  vpc_security_group_ids  = aws_security_group.allow_rds.id
+  allocated_storage           = var.DB_SIZE
+  storage_type                = "gp2"
+  engine                      = var.DB_ENGINE
+  engine_version              = var.DB_ENGINE_VERSION
+  instance_class              = var.DB_INSTANCE
+  name                        = var.DB_NAME
+  username                    = var.DB_USER
+  password                    = var.DB_PASS
+  parameter_group_name        = aws_db_parameter_group.default.id
+  skip_final_snapshot         = !var.NEED_FINAL_SNAPSHOT
+  identifier                  = "${var.DB_NAME}-db"
+  db_subnet_group_name        = aws_db_subnet_group.default.id
+  vpc_security_group_ids      = aws_security_group.allow_rds.id
   tags = {
      Name         = "${var.PROJECT_NAME}-${var.PROJECT_ENV}-RDS instance"
      created_by   = "Terraform"
