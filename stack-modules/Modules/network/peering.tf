ac30fb17630daef6c0188a95f7eefd024915d0dd
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-21 04:55:07 +0100

    --- Auto-Git Commit ---

---
+++ stack-modules/Modules/network/peering.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc_peering_connection" "peer" {
+  vpc_id        = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = aws_vpc.peer.id
+  peer_owner_id = data.aws_caller_identity.peer.account_id
+  peer_region   = "us-west-2"
+  auto_accept   = false
+
+  tags = {
+    Side = "Requester"
+  }
+}
