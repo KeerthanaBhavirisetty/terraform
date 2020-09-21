d965377503b31963b4e188c38ed506189a14fc5e
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-21 06:11:51 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/database/rds.tf
+++ stack-modules/Modules/database/rds.tf
@@ -22,23 +22,23 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_security_group" "allow_rds" {
-  name        = "mariadb_rds"
-  description = "Allow RDS inbound traffic"
-  vpc_id      = var.VPC_ID
+  name              = "mariadb_rds"
+  description       = "Allow RDS inbound traffic"
+  vpc_id            = var.VPC_ID
 
   ingress {
-    description = "RDS from VPC"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "TCP"
-    cidr_blocks = [var.VPC_CIDR, var.DEFAULT_VPC_CIDR]
+    description     = "RDS from VPC"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "TCP"
+    cidr_blocks     = [var.VPC_CIDR, var.DEFAULT_VPC_CIDR]
   }
 
   tags = {
-     Name         = "${var.PROJECT_NAME}-${var.PROJECT_ENV}-SecurityGroup"
-     created_by   = "Terraform"
-     PROJECT_NAME = var.PROJECT_NAME
-     ENVIRONMENT  = var.PROJECT_ENV
+     Name                     = "${var.PROJECT_NAME}-${var.PROJECT_ENV}-SecurityGroup"
+     created_by               = "Terraform"
+     PROJECT_NAME             = var.PROJECT_NAME
+     ENVIRONMENT              = var.PROJECT_ENV
   }
 }
 
