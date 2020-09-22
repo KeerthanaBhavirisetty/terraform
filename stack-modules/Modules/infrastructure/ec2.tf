7f38310774d3976fcbae5ef3ecd3b438ea4bc305
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-22 20:54:31 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/infrastructure/ec2.tf
+++ stack-modules/Modules/infrastructure/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
     ami             = data.aws_ami.myami.id
     instance_type   = var.INSTANCE_TYPE
     key_name        = var.KEYPAIR_NAME
-    subnet_id       = var.PUBLIC_SUBNETS
+    subnet_id       = element(var.PUBLIC_SUBNETS,count.index)
 
     tags = {
       Name = "HelloWorld"
