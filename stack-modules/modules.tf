e7a4ff98010193ffbbadbf3f9ae714c47fe0d95e
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-22 06:30:18 +0100

    --- Auto-Git Commit ---

--- stack-modules/modules.tf
+++ stack-modules/modules.tf
@@ -25,10 +25,10 @@ module "database" {
 }
 
 module "infra" {
-  source                = ".Modules/infrastructure"
+  source                = "./Modules/infrastructure"
   PROJECT_NAME          = var.PROJECT_NAME
   PROJECT_ENV           = var.PROJECT_ENV
   VPC_CIDR              = var.VPC_CIDR
-  
+
 
 }
