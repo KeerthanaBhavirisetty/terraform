9f4bee78c6910dca176a736a6f80ab83ba8c5eee
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-22 06:15:35 +0100

    --- Auto-Git Commit ---

--- stack-modules/modules.tf
+++ stack-modules/modules.tf
@@ -7,28 +7,28 @@ module "network" {
 }
 
 module "database" {
-  source               = "./Modules/database"
-  DB_SIZE              =  var.DB_SIZE
-  DB_ENGINE            =  var.DB_ENGINE
-  DB_ENGINE_VERSION    =  var.DB_ENGINE_VERSION
-  NEED_FINAL_SNAPSHOT  =  var.NEED_FINAL_SNAPSHOT
-  DB_USER              =  var.DB_USER
-  DB_NAME              =  var.DB_NAME
-  DB_PASS              =  var.DB_PASS
-  DB_INSTANCE          =  var.DB_INSTANCE
-  PRIVATE_SUBNETS      = module.network.private-subnets
-  PROJECT_NAME         = var.PROJECT_NAME
-  PROJECT_ENV          = var.PROJECT_ENV
-  VPC_CIDR             = var.VPC_CIDR
-  VPC_ID               = module.network.studentapp_vpcid
-  DEFAULT_VPC_CIDR     = module.network.default_vpc_cidr
+  source                = "./Modules/database"
+  DB_SIZE               =  var.DB_SIZE
+  DB_ENGINE             =  var.DB_ENGINE
+  DB_ENGINE_VERSION     =  var.DB_ENGINE_VERSION
+  NEED_FINAL_SNAPSHOT   =  var.NEED_FINAL_SNAPSHOT
+  DB_USER               =  var.DB_USER
+  DB_NAME               =  var.DB_NAME
+  DB_PASS               =  var.DB_PASS
+  DB_INSTANCE           =  var.DB_INSTANCE
+  PRIVATE_SUBNETS       = module.network.private-subnets
+  PROJECT_NAME          = var.PROJECT_NAME
+  PROJECT_ENV           = var.PROJECT_ENV
+  VPC_CIDR              = var.VPC_CIDR
+  VPC_ID                = module.network.studentapp_vpcid
+  DEFAULT_VPC_CIDR      = module.network.default_vpc_cidr
 }
 
 module "infra" {
-  source               = ".Modules/infrastructure"
-  PROJECT_NAME         = var.PROJECT_NAME
-  PROJECT_ENV          = var.PROJECT_ENV
-  VPC_CIDR             = var.VPC_CIDR
-  AZ                   = var.AZ 
+  source                = ".Modules/infrastructure"
+  PROJECT_NAME          = var.PROJECT_NAME
+  PROJECT_ENV           = var.PROJECT_ENV
+  VPC_CIDR              = var.VPC_CIDR
+  AZ                    = var.AZ 
 
 }
