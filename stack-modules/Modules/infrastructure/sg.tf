2073cc7831a846087c7be8b98f6c8642b3c48d4b
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-22 21:41:10 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/infrastructure/sg.tf
+++ stack-modules/Modules/infrastructure/sg.tf
@@ -1,18 +1,25 @@
-resource "aws_security_group" "allow_rds" {
-  name              = "mariadb_rds"
-  description       = "Allow RDS inbound traffic"
+resource "aws_security_group" "allow_ec2" {
+  name              = "ec2-sg"
+  description       = "Allow EC2 inbound traffic"
   vpc_id            = var.VPC_ID
 
   ingress {
-    description     = "RDS from VPC"
-    from_port       = 3306
-    to_port         = 3306
+    from_port       = 22
+    to_port         = 22
     protocol        = "TCP"
     cidr_blocks     = [var.VPC_CIDR, var.DEFAULT_VPC_CIDR]
   }
 
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "TCP"
+    description     = "Application open to outside world"
+    cidr_blocks     = [var.VPC_CIDR, var.DEFAULT_VPC_CIDR]
+  }
+
   tags = {
-     Name                     = "${var.PROJECT_NAME}-${var.PROJECT_ENV}-SecurityGroup"
+     Name                     = "${var.PROJECT_NAME}-${var.PROJECT_ENV}-EC2-SecurityGroup"
      created_by               = "Terraform"
      PROJECT_NAME             = var.PROJECT_NAME
      ENVIRONMENT              = var.PROJECT_ENV
