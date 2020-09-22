c6bddd47e09452dbd3fba89a218ccf97b6ccd798
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-22 21:43:10 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/infrastructure/ec2.tf
+++ stack-modules/Modules/infrastructure/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
     instance_type               = var.INSTANCE_TYPE
     key_name                    = var.KEYPAIR_NAME
     subnet_id                   = element(var.PUBLIC_SUBNETS,count.index)
-    vpc_security_group_ids      =   [aws_security_group.allow_ec2.id]
+    vpc_security_group_ids      = [aws_security_group.allow_ec2.id]
 
     tags = {
         Name                    = "${var.PROJECT_NAME}-${var.PROJECT_ENV}-Instance-${count.index+1}"
