1ea1668e164f4d18f16e952284f6a8d4b4837b86
KeerthanaBhavirisetty <yaswanthbotcha@gmail.com>
2020-09-23 05:57:18 +0100

    --- Auto-Git Commit ---

--- stack-modules/Modules/infrastructure/ec2.tf
+++ stack-modules/Modules/infrastructure/ec2.tf
@@ -20,7 +20,7 @@ resource "null_resource" "connect-to-ec2" {
         type            = "ssh"
         user            = "centos"
         private_key     = file("/opt/devops.pem")
-        host            = element(aws_instance.nodes.private_ip, 0)  
+        host            = element(aws_instance.nodes.*.private_ip, 0)  
     }
 
     provisioner "file" {
